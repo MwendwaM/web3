392c4ff0ed767d39ad2c2f0ffb60a4a9cc8b0884
MwendwaM <fisher1671999@gmail.com>
2023-04-23 06:41:44 -0700

    first commit

--- blockchain/vending.js
+++
@@ -1,9 +0,0 @@
-const abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"donutBalances","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getVendingMachineBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"purchase","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"restock","outputs":[],"stateMutability":"nonpayable","type":"function"}]
-
-const VendingMachineContract = web3 => {
-    return new web3.eth.Contract(
-        abi, "0xdE9680FD7a3227697B61843dbCB6b7E0Bc0b745E"
-    )
-}
-
-export default VendingMachineContract
